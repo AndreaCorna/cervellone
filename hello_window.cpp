be58b3affc24a2caa060fadb8b8dee353e1072e5
Andrea Corna <andrea.corna.ac.91@gmail.com>
2013-09-22 20:15:39 +0200

    aggiunte domande immagini

--- hello_window.cpp
+++ hello_window.cpp
@@ -6,6 +6,9 @@ hello_window::hello_window(QWidget *parent) :
     ui(new Ui::hello_window)
 {
     ui->setupUi(this);
+    QPixmap image("image/intro.jpg");
+    ui->label_2->setPixmap(image);
+    ui->label_2->setVisible(true);
 }
 
 hello_window::~hello_window()
